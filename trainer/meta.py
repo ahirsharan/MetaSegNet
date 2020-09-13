cfac2671acd7a20873f7e9169e5000c2140b53ea
Gursharan Ahir <ahir.sharan@gmail.com>
2020-09-13 17:24:48 +0530

    Update meta.py

--- trainer/meta.py
+++ trainer/meta.py
@@ -277,7 +277,7 @@ class MetaTrainer(object):
                 
                 x1 = im_test[j].detach().cpu()
                 y1 = out_test[j].detach().cpu()
-                z1 = Gte[j].detach().cpu() #Do Changes
+                z1 = Gte[j].detach().cpu()
                 z1 = torch.argmax(z1,axis=1)
                 
                 m=int(sqrt(z1.shape[0])) 
