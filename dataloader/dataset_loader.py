809236b9a604ff9537f81c857a8a4ff789ef31b3
Gursharan Ahir <ahir.sharan@gmail.com>
2020-09-13 19:46:10 +0530

    Update dataset_loader.py

--- dataloader/dataset_loader.py
+++ dataloader/dataset_loader.py
@@ -59,8 +59,8 @@ class DatasetLoader(Dataset):
             transforms.ToTensor()])
 
         self.btransform = transforms.Compose([
-            transforms.Resize(image_size//2 + 10),
-            transforms.CenterCrop(image_size//2)])
+            transforms.Resize(image_size + 10),
+            transforms.CenterCrop(image_size)])
 
         self.colormap = build_colormap2label()  
     
