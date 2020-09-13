38df0dbe2bef9684c223de7f99c24dab8a356816
Gursharan Ahir <ahir.sharan@gmail.com>
2020-09-13 19:20:20 +0530

    Update downlabel.py

--- utils/downlabel.py
+++ utils/downlabel.py
@@ -1,7 +1,15 @@
 import torch
 
+def index(arr,x):
+    in=-1
+    for i in range(len(arr)):
+        if(arr[i]==x):
+            in=i
+            break
+    return in
+
 #Bring the labels from global labels to (0 to K-1) for meta tasks
-def downlabel(labels,K):
+def downlabel(labels,K,category):
     visited=[False]*100
     uniq=[]
     for x in labels:
@@ -10,11 +18,26 @@ def downlabel(labels,K):
                 if(visited[x[i][j]] == False):
                     visited[x[i][j]]=True
                     uniq.append(int(x[i][j]))
+    uniqn=[]
+    uniqn.append(uniq[0]) #Included Background Class    
+    
+    if(category=='Train'):
+        for i in range(1,K):
+            uniqn.append(uniq[i])
+    else:
+        for i in range(len(uniq)-1,len(uniq)-K,-1):
+            uniqn.append(uniq[i])       
+    
+    uniqn.sort()
     for x in labels:
         for i in range(x.shape[0]):
             for j in range(x.shape[1]):
-                x[i][j]=uniq.index(int(x[i][j]))
+                in=index(uniqn,int(x[i][j]))
+                if(in!=-1):
+                    x[i][j]=in
+                else:
+                    x[i][j]=0
     
-    print(torch.unique(x))
+    #print(torch.unique(x))
     return labels
   
