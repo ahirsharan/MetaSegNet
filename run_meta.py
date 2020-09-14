3e18c894446bba656fd981b80ddd4319d7365ff4
Gursharan Ahir <ahir.sharan@gmail.com>
2020-09-14 18:47:48 +0530

    Update run_meta.py

--- run_meta.py
+++ run_meta.py
@@ -3,7 +3,7 @@ import os
 
 # N shot and K way
 def run_exp(num_batch=50, N=1, Q=1, lr=0.001, update_step=20):
-    max_epoch = 40
+    max_epoch = 2
     step_size = 20
     K = 2            #Backround class not included. Adjust accordingly further.
     gpu = 0
@@ -22,4 +22,4 @@ def run_exp(num_batch=50, N=1, Q=1, lr=0.001, update_step=20):
     os.system(the_command + ' --phase=train')
     os.system(the_command + ' --phase=test')
 
-run_exp(num_batch=1000, N=10, Q=1, lr=0.001, update_step=100)
+run_exp(num_batch=2, N=10, Q=1, lr=0.001, update_step=50)
