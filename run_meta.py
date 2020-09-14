85dee051309528b961b5458433383b7c01b5d461
Gursharan Ahir <ahir.sharan@gmail.com>
2020-09-14 11:58:57 +0530

    Update run_meta.py

--- run_meta.py
+++ run_meta.py
@@ -22,4 +22,4 @@ def run_exp(num_batch=50, N=1, Q=1, lr=0.001, update_step=20):
     os.system(the_command + ' --phase=train')
     os.system(the_command + ' --phase=test')
 
-run_exp(num_batch=50, N=1, Q=1, lr=0.001, update_step=20)
+run_exp(num_batch=50, N=1, Q=1, lr=0.001, update_step=1000)
