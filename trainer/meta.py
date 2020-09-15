7c5f3c0ea80aaa14e06a826dcf7cf91fb7d360e1
Gursharan Ahir <ahir.sharan@gmail.com>
2020-09-15 13:09:58 +0530

    Update meta.py

--- trainer/meta.py
+++ trainer/meta.py
@@ -211,7 +211,7 @@ class MetaTrainer(object):
                 self.save_model('max_iou')
                 
             # Save model every 5 epochs
-            if epoch % 5 == 0:
+            if epoch % 2 == 0:
                 self.save_model('epoch'+str(epoch))
 
             # Update the logs
