a82310875b01979b54183bad5dcfe384823991f7
Gursharan Ahir <ahir.sharan@gmail.com>
2020-09-16 21:39:55 +0530

    Update run_meta.py

--- run_meta.py
+++ run_meta.py
@@ -3,17 +3,17 @@ import os
 
 # N shot and K way
 def run_exp(num_batch=50, N=1, Q=1, lr=0.001, update_step=20):
-    max_epoch = 80
+    max_epoch = 40
     step_size = 20
     K = 2            #Background class not included. Adjust accordingly further.
     gpu = 1
     
-    dataset='COCO'
-    dataset_dir='../Datasets/COCOAug/'
+    #dataset='COCO'
+    #dataset_dir='../Datasets/COCOAug/'
     #num_batch is episodes   
     the_command = 'python3 main.py' \
-        + ' --dataset=' +str(dataset) \
-        + ' --dataset_dir=', str(dataset_dir) \
+        #+ ' --dataset=' +str(dataset) \
+        #+ ' --dataset_dir=', str(dataset_dir) \
         + ' --max_epoch=' + str(max_epoch) \
         + ' --num_batch=' + str(num_batch) \
         + ' --train_query=' + str(N) \
@@ -27,4 +27,4 @@ def run_exp(num_batch=50, N=1, Q=1, lr=0.001, update_step=20):
     os.system(the_command + ' --phase=train')
     os.system(the_command + ' --phase=test')
 
-run_exp(num_batch=500, N=1, Q=2, lr=0.001, update_step=100)
+run_exp(num_batch=1000, N=5, Q=5, lr=0.001, update_step=100)
