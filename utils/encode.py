f5c20cf9bdbfad4860167a3d676625132cf365c7
Gursharan Ahir <ahir.sharan@gmail.com>
2020-09-16 20:25:38 +0530

    Update encode.py

--- utils/encode.py
+++ utils/encode.py
@@ -6,13 +6,17 @@ VOC_COLORMAP = [[0, 0, 0], [128, 0, 0], [0, 128, 0], [128, 128, 0],
                 [64, 0, 0], [192, 0, 0], [64, 128, 0], 
                 [0, 64, 0], [128, 64, 0], [0, 192, 0], [128, 192, 0],
                 [0, 64, 128], [192, 128, 0],
-                [64, 0, 128], [192, 0, 128], [64, 128, 128], [192, 128, 128]]
+                [64, 0, 128], [192, 0, 128], [64, 128, 128], [192, 128, 128],
+                [0, 64, 0], [128, 64, 0], [0, 192, 0], [128, 192, 0], [0, 64, 128],
+                [0 ,0 ,192],[0,0,64],[64,64,128],[64,64,64],[192,192,192],[64,64,0]]
 
-#Corresponding classes
+#Corresponding classes for PASCAL
 VOC_CLASSES = ['background', 'aeroplane', 'bicycle', 'bird', 'boat',
                'bottle', 'bus', 'car', 'cat', 'chair', 'cow',
                'potted plant', 'sheep', 'sofa', 'train', 'tv/monitor',
               'diningtable', 'dog', 'horse', 'motorbike', 'person']
+
+COCO_CLASSES=['background','person','bicycle','motorcycle','airplane','train','truck','boat','horse','sheep','cow','bear','zebra','giraffe','chair','couch','potted plant','bed','toilet','car','bus','bird','cat','dog','elephant','dining table']
 
 def build_colormap2label():
     """Build an RGB color to label mapping for segmentation."""
