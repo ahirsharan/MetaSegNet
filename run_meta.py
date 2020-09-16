43fab287a2c12420112908ecc08b464aa1fc6eaf
Gursharan Ahir <ahir.sharan@gmail.com>
2020-09-16 20:38:19 +0530

    Update run_meta.py

--- run_meta.py
+++ run_meta.py
@@ -9,7 +9,7 @@ def run_exp(num_batch=50, N=1, Q=1, lr=0.001, update_step=20):
     gpu = 1
     
     dataset='COCO'
-    dataset_dir='..\Datasets\COCOAug\'
+    dataset_dir='../Datasets/COCOAug/'
     #num_batch is episodes   
     the_command = 'python3 main.py' \
         + ' --dataset=' +str(dataset) \
