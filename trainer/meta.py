337a01ae2b6a1568cf49a682bc1968c3a01418db
Gursharan Ahir <ahir.sharan@gmail.com>
2020-09-29 01:46:47 +0530

    Update meta.py

--- trainer/meta.py
+++ trainer/meta.py
@@ -23,11 +23,11 @@ class MetaTrainer(object):
     """The class that contains the code for the meta-train phase and meta-eval phase."""
     def __init__(self, args):
         # Set the folder to save the records and checkpoints
-        save_image_dir='../results2/'
+        save_image_dir='../results3/'
         if not osp.exists(save_image_dir):
             os.mkdir(save_image_dir)        
         
-        log_base_dir = '../logs2/'
+        log_base_dir = '../logs3/'
         if not osp.exists(log_base_dir):
             os.mkdir(log_base_dir)
         meta_base_dir = osp.join(log_base_dir, 'meta')
