0912327f4244a0897f9b8ac004d94b32ec386589
Gursharan Ahir <ahir.sharan@gmail.com>
2020-09-29 19:57:50 +0530

    Update run_meta.py

--- run_meta.py
+++ run_meta.py
@@ -10,6 +10,15 @@ def run_exp(num_batch=50, N=1, Q=1, lr=0.001, update_step=20):
     
     dataset='COCO'
     dataset_dir='../Datasets/COCOAug/'
+    
+    '''
+    dataset='FSS1000'
+    dataset_dir='../Datasets/FSS1000Aug/'
+
+    dataset='PASCAL'
+    dataset_dir='../Datasets/Pascal5Aug/'
+    ''''
+    
     #num_batch is episodes   
     the_command = 'python3 main.py' \
         + ' --dataset=' +str(dataset) \
