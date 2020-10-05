63be1bdcf9d868739e78b37b32731b2dbc7c8b19
Gursharan Ahir <ahir.sharan@gmail.com>
2020-10-05 14:03:50 +0530

    Update run_meta.py

--- run_meta.py
+++ run_meta.py
@@ -6,7 +6,7 @@ def run_exp(num_batch=50, N=1, Q=1, lr=0.001, update_step=20):
     max_epoch = 80
     step_size = 20
     K = 2            #Background class not included. Adjust accordingly further.
-    gpu = 1
+    gpu = 0
     mtype = 'Net'
     valdata = 'No'
     
@@ -43,4 +43,4 @@ def run_exp(num_batch=50, N=1, Q=1, lr=0.001, update_step=20):
 
 #N shots should be changed
 #Q can be kept the same ie 2
-run_exp(num_batch=500, N=5, Q=2, lr=0.001, update_step=100)
+run_exp(num_batch=500, N=1, Q=2, lr=0.0005, update_step=100)
